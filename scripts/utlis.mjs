9731ad426f301e1b882ea1e6993db8e2cec761e1
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-10-01 17:28:39 -0600

    fixing paths

--- scripts/utlis.mjs
+++ scripts/utlis.mjs
@@ -9,11 +9,11 @@ export async function loadTemplate(path) {
 }   
 
 export async function loadMainTemplates() {
-  const headerTemplate = await loadTemplate("../partials/header.html");
+  const headerTemplate = await loadTemplate("partials/header.html");
   const headerElement = document.querySelector("header");
   renderWithTemplate(headerTemplate, headerElement);
 
-  const footerTemplate = await loadTemplate("../partials/footer.html");
+  const footerTemplate = await loadTemplate("partials/footer.html");
   const footerElement = document.querySelector("footer");
   renderWithTemplate(footerTemplate,footerElement);
 }
